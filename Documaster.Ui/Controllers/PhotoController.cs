82f7769fe184ac68b4bb74c9825505d9dbf03eaf
Florin Constantinescu <nyrolphf16@gmail.com>
2019-04-02 18:11:15 +0300

    Small fixes for photo uploading

--- Documaster.Ui/Controllers/PhotoController.cs
+++ Documaster.Ui/Controllers/PhotoController.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Web;
+﻿using System.Web;
 using System.Web.Mvc;
-using Documaster.Business.Services;
-using Documaster.Model.Enums;
 using System.IO;
 
 namespace Documaster.Ui.Controllers
 {
     public class PhotoController : Controller
     {
-
         [HttpGet]
         public ActionResult Index()
         {
@@ -23,10 +16,10 @@ namespace Documaster.Ui.Controllers
         [HttpPost]
         public ActionResult Index(HttpPostedFileBase fileUpload)
         {
-            if (fileUpload.ContentLength > 0)
+            if (fileUpload != null && fileUpload.ContentLength > 0)
             {
-                string filename = Path.GetFileName(fileUpload.FileName);
-                string filepath = Path.Combine(Server.MapPath("~/Images/"), filename);
+                var filename = Path.GetFileName(fileUpload.FileName);
+                var filepath = Path.Combine(Server.MapPath("~/Images/"), filename);
                 fileUpload.SaveAs(filepath);
             }
             return RedirectToAction("Welcome", "Project");
