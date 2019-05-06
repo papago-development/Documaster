abe3d5908d5e7b19a18d9d64b4119853626b65eb
Maria-Alexandra Irimescu <irimescu.mariaalexandra@gmail.com>
2019-05-06 17:46:22 +0300

    Added new method to Global.asax file

--- Documaster.Ui/Global.asax.cs
+++ Documaster.Ui/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Net;
 
 namespace Documaster.Ui
 {
@@ -16,6 +17,20 @@ namespace Documaster.Ui
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            var ex = Server.GetLastError().GetBaseException();
+
+            var httpContext = ((MvcApplication)sender).Context;
+            httpContext.Response.Clear();
+            httpContext.Response.ContentType = "text/plain";
+            httpContext.Response.TrySkipIisCustomErrors = true;
+            Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            Response.Write(ex);
+
+            Server.ClearError();
         }
     }
 }
