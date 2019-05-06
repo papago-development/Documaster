5e47fb5e68fe38e75a2f8332233cac4af2324cac
Maria-Alexandra Irimescu <irimescu.mariaalexandra@gmail.com>
2019-05-06 11:04:51 +0300

    Added jquery-ui to BundleConfig file

--- Documaster.Ui/App_Start/BundleConfig.cs
+++ Documaster.Ui/App_Start/BundleConfig.cs
@@ -28,6 +28,7 @@ namespace Documaster.Ui
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/themes/base/jquery-ui.css",
                       "~/Content/Site.css"));
         }
     }
