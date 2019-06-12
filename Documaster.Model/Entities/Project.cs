fd7fad82ef08097494ccf65c7c795ef15ddbea6e
Maria-Alexandra Irimescu <irimescu.mariaalexandra@gmail.com>
2019-06-12 12:03:26 +0300

    Fixed problem with sorting on ProjectStatus property

--- Documaster.Model/Entities/Project.cs
+++ Documaster.Model/Entities/Project.cs
@@ -26,7 +26,7 @@ namespace Documaster.Model.Entities
         [Required(ErrorMessage = "Numele nu poate fi gol")]
          public override string Name { get; set; }
 
-        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:dd.mm.yyyy}", ApplyFormatInEditMode = true)]
         public DateTime Created { get; set; }
          
         public virtual Customer Customer { get; set; }
