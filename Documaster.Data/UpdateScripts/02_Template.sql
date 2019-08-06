e71e07a6dc7005049fcf4b2ecb10321631aec7a6
Florin Constantinescu <nyrolphf16@gmail.com>
2019-08-06 15:22:32 +0300

    Removed App_Browsers. Corrected migration script

--- Documaster.Data/UpdateScripts/02_Template.sql
+++ Documaster.Data/UpdateScripts/02_Template.sql
@@ -1,4 +1,8 @@
-﻿CREATE TABLE dbo.CustomizeTab (
+﻿ALTER TABLE dbo.Project
+	ADD AllowNotification BIT NOT NULL CONSTRAINT DF_Project_AllowNotification DEFAULT (0)
+GO
+
+CREATE TABLE dbo.CustomizeTab (
     Id INT IDENTITY(1, 1) NOT NULL
     ,[Name] NVARCHAR(200) NOT NULL
     ,[Type] NVARCHAR(25) NOT NULL
@@ -17,13 +21,59 @@ CREATE UNIQUE NONCLUSTERED INDEX UX_CustomizeTab_Name
      ON dbo.CustomizeTab ([Name])
 GO
 
--- Added column CustomizeTabId to OutputDocuments table --
+INSERT dbo.CustomizeTab VALUES
+	('Documente', 'DisplayDocuments', 1, GETDATE(), GETDATE()),
+	('Cert. urbanism', 'DisplayDocuments', 2, GETDATE(), GETDATE()),
+	('Avize', 'OutputDocuments', 3, GETDATE(), GETDATE()),
+	('Foto Teren', 'DisplayDocuments', 4, GETDATE(), GETDATE()),
+	('Foto 3D', 'DisplayDocuments', 5, GETDATE(), GETDATE()),
+	('Desene', 'DisplayDocuments', 6, GETDATE(), GETDATE()),
+	('Note', 'Notes', 7, GETDATE(), GETDATE())
+GO
+
 ALTER TABLE OutputDocument 
      ADD CustomizeTabId INT 
 GO
 
-UPDATE OutputDocument
-       SET CustomizeTabId=1
+/**/
+UPDATE dbo.OutputDocument
+SET CustomizeTabId = 1
+WHERE DocumentType = 'Document'
+GO
+
+/**/
+UPDATE dbo.OutputDocument
+SET CustomizeTabId = 2
+WHERE DocumentType = 'SpecialDocumentType'
+GO
+
+/**/
+UPDATE dbo.OutputDocument
+SET CustomizeTabId = 3
+WHERE DocumentType = 'OutputDocument'
+GO
+
+/**/
+UPDATE dbo.OutputDocument
+SET CustomizeTabId = 4
+WHERE DocumentType = 'GroundDrawing'
+GO
+
+/**/
+UPDATE dbo.OutputDocument
+SET CustomizeTabId = 5
+WHERE DocumentType = 'Photo'
+GO
+
+/**/
+UPDATE dbo.OutputDocument
+SET CustomizeTabId = 6
+WHERE DocumentType = 'Drawing'
+GO
+
+UPDATE dbo.OutputDocument
+SET DocumentType = 'DisplayDocuments'
+WHERE RequirementId IS NULL
 GO
 
 ALTER TABLE OutputDocument ALTER COLUMN CustomizeTabId INT NOT NULL
@@ -38,4 +88,35 @@ GO
 
 ALTER TABLE dbo.OutputDocument 
      CHECK CONSTRAINT FK_OutputDocument_CustomizeTabId_CustomizeTab_Id
+GO
+
+
+CREATE TABLE dbo.ProjectTemplate (
+	Id INT IDENTITY(1,1) NOT NULL
+   ,ProjectId INT NOT NULL
+   ,[Name] NVARCHAR(200) NOT NULL
+   ,CreationDate DATETIME NOT NULL
+   ,LastUpdate DATETIME NOT NULL
+   ,[Text] VARCHAR(MAX) NOT NULL
+)
+GO
+
+ALTER TABLE dbo.ProjectTemplate
+	ADD CONSTRAINT PK_ProjectTemplate
+	PRIMARY KEY CLUSTERED (Id)
+GO
+
+ALTER TABLE dbo.ProjectTemplate WITH CHECK
+	ADD CONSTRAINT FK_ProjectTemplate_ProjectId_Project_Id
+	FOREIGN KEY (ProjectId)
+	REFERENCES dbo.Project (Id)
+	ON DELETE CASCADE
+GO
+
+ALTER TABLE dbo.ProjectTemplate
+	CHECK CONSTRAINT FK_ProjectTemplate_ProjectId_Project_Id
+GO
+
+CREATE UNIQUE NONCLUSTERED INDEX UX_ProjectTemplate_ProjectId_Name
+	ON dbo.ProjectTemplate (ProjectId, [Name])
 GO
